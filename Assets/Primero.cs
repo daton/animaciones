52763409f7d7236686cb4ac00ab0dbcd7c9df803
Juan Carlos <rapidclimate@gmail.com>
2019-08-12 10:15:30 -0500

    perro

---
+++ Assets/Primero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Primero : MonoBehaviour
+{
+
+    GameObject objeto;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // transform.Rotate(Vector3.up*Time.deltaTime*30, Space.World); 
+    }
+    private void OnCollisionEnter(Collision other) {
+        
+    }
+}
